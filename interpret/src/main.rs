7e2c31e4b3a69c07ace65935e63dbd75c18df55c
manen <49951816+manen@users.noreply.github.com>
2025-05-24 18:47:22 +0200

    lil example

--- interpret/src/main.rs
+++ interpret/src/main.rs
@@ -1,3 +1,11 @@
+use interpret::Context;
+use langlib::{Expr, Function, Reach, Value};
+
 fn main() {
-	unimplemented!("interpret main")
+	let ctx = Context::new([("num".into(), Value::f32(2.2))]);
+
+	let expr = Expr::Add(Reach::Named("num".into()), Reach::Value(Value::i32(4)));
+	let out = ctx.resolve_expr(&expr).unwrap();
+
+	println!("num + 4 = {out:?}");
 }
